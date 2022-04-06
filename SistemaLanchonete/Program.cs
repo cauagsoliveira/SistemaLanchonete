c400b9c345b242b0e5744120f79c43032c86a137
Cauã Guedes <cauagsotto8@gmail.com>
2022-04-05 22:44:18 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaLanchonete/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class lanche
+{
+
+
+
+}
+namespace SistemaLanchonete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
